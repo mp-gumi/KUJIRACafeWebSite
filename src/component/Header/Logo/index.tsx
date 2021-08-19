b84b9619d9b17c07c32b11a18c48d72a8fdc0cc0
mp-gumi <basil725woodstock@gmail.com>
2021-08-19 10:19:14 +0900

    logo画像を設定

--- src/component/Header/Logo/index.tsx
+++ src/component/Header/Logo/index.tsx
@@ -1,7 +1,12 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
+import logo from "src/component/Header/Logo/logo.png";
 
 export const Logo = () => {
-  return <div>logo</div>;
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
 };
