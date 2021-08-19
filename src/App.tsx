4d548da34c0bfb7bc4b74cd91bf3e3cb7359ec66
mp-gumi <basil725woodstock@gmail.com>
2021-08-19 10:51:11 +0900

    Footerの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
 import { Header } from "src/component/Header";
+import { Footer } from "src/component/Footer";
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/component/Footer/index.tsx
@@ -0,0 +1,11 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <small>&copy;Copyright KUJIRA Cafe. All rights reserved.</small>
+    </div>
+  );
+};
