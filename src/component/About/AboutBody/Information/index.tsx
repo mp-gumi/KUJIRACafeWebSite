0312aa0dfec04b4d9a34645809b9dddeaa7ce594
mp-gumi <basil725woodstock@gmail.com>
2021-08-20 11:50:24 +0900

    About Information 配列を用いてtableを作成

--- src/component/About/AboutBody/Information/index.tsx
+++ src/component/About/AboutBody/Information/index.tsx
@@ -1,7 +1,85 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
+import { Link } from "react-router-dom";
 import { textBlue } from "src/component/Home/HomeBody";
+
+const addressContent = (
+  <div>
+    〒199-9999 或留県九寺楽市九寺楽町3-30-8（
+    <Link
+      to="/access"
+      css={css`
+        color: #d25833;
+        &:hover {
+          color: #e3937a;
+        }
+      `}
+    >
+      地図
+    </Link>
+    ）
+  </div>
+);
+
+const reserveContent = (
+  <div>
+    <p>
+      ご予約は、お電話もしくは
+      <Link
+        to="/contact"
+        css={css`
+          color: #d25833;
+          &:hover {
+            color: #e3937a;
+          }
+        `}
+      >
+        お問い合わせフォーム
+      </Link>
+      より受け付けております。ご予約希望日時と人数をお知らせください。
+    </p>
+    <p>
+      ※フォームからのご予約にはお時間がかかる場合がございますので、ご了承ください。
+    </p>
+  </div>
+);
+
+const informationList = [
+  { name: "住所", content: addressContent },
+  { name: "電話番号", content: "09-9280-2611" },
+  { name: "営業時間", content: "10:00～22:00" },
+  { name: "定休日", content: "水曜日・日曜日" },
+  { name: "ご予約", content: reserveContent },
+];
+const infomationTable = informationList.map((infomation) => {
+  return (
+    <tr>
+      <th
+        css={css`
+          border: 1px solid #bec2c7;
+          white-space: nowrap;
+          padding: 8px;
+          line-height: 1.6;
+          color: ${textBlue};
+          font-weight: bold;
+          background-color: #dde2ea;
+        `}
+      >
+        {infomation.name}
+      </th>
+      <td
+        css={css`
+          border: 1px solid #bec2c7;
+          padding: 8px;
+          line-height: 1.6;
+        `}
+      >
+        {infomation.content}
+      </td>
+    </tr>
+  );
+});
 
 export const Information = () => {
   return (
@@ -18,30 +96,14 @@ export const Information = () => {
       >
         店舗情報
       </h2>
-      <table>
-        <tr>
-          <th>住所</th>
-          <td>〒199-9999 或留県九寺楽市九寺楽町3-30-8（地図）</td>
-        </tr>
-        <tr>
-          <th>電話番号</th>
-          <td>09-9280-2611</td>
-        </tr>
-        <tr>
-          <th>営業時間</th>
-          <td>10:00~20:00</td>
-        </tr>
-        <tr>
-          <th>定休日</th>
-          <td>水曜日・日曜日</td>
-        </tr>
-        <tr>
-          <th>ご予約</th>
-          <td>
-            ご予約は、お電話もしくはお問い合わせフォームより受け付けております。ご予約希望日時と人数をお知らせください。
-            ※フォームからのご予約にはお時間がかかる場合がございますので、ご了承ください。
-          </td>
-        </tr>
+      <table
+        css={css`
+          border-collapse: collapse;
+          text-align: left;
+          font-size: 16px;
+        `}
+      >
+        {infomationTable}
       </table>
     </div>
   );
