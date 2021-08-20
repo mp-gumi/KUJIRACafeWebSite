df9b9bf0018a490bdf135e8820438d17cd36aa5e
mp-gumi <basil725woodstock@gmail.com>
2021-08-20 14:32:55 +0900

    Menu itemの配列を作成し、表示した。 画面幅が横幅にあっていない

--- src/component/Menu/MenuBody/index.tsx
+++ src/component/Menu/MenuBody/index.tsx
@@ -1,7 +1,35 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
+import cassisOrangeJuice from "src/component/Menu/MenuBody/menu-photo1.jpg";
+import nutsTarte from "src/component/Menu/MenuBody/menu-photo2.jpg";
+import plumTarte from "src/component/Menu/MenuBody/menu-photo3.jpg";
 import { textBlue } from "src/component/Home/HomeBody";
+import { maxWidth } from "src/App";
+
+const itemList = [
+  {
+    name: "カシスとオレンジのジュース",
+    image: cassisOrangeJuice,
+    description:
+      "リキュールを使ったカクテルでおなじみのカシスは、ポリフェノールやビタミンが豊富なベリー系のフルーツです。オレンジとともにジューサーにかけて、爽やかなドリンクに仕上げました。",
+    price: 480,
+  },
+  {
+    name: "ナッツのタルト",
+    image: nutsTarte,
+    description:
+      "ピーカンナッツにアーモンド、くるみ、ヘーゼルナッツ、パンプキンシード。5種類のナッツを贅沢にトッピングした当店の定番タルトです。黒糖を使用したコクのある甘味は、コーヒーのお供にぴったりです。",
+    price: 550,
+  },
+  {
+    name: "太陽プラムのタルト",
+    image: plumTarte,
+    description:
+      "今月の旬のタルトはこちら。山梨産のプラム「太陽」を使用しています。太陽は、完熟すると酸味が抜けて、驚くほど甘くなります。クリームチーズを混ぜ込んだフィリングとも相性抜群。この時期だけの味、ぜひお試しください.",
+    price: 600,
+  },
+];
 
 export const MenuBody = () => {
   return (
@@ -18,6 +46,52 @@ export const MenuBody = () => {
       >
         メニュー
       </h2>
+      {itemList.map((item) => {
+        return (
+          <div
+            css={css`
+              display: flex;
+              border-bottom: 1px dotted #bec2c7;
+              font-size: 16px;
+              line-height: 1.6;
+              padding: 20px 8px;
+              @media (max-width: ${maxWidth}) {
+                flex-direction: column;
+              }
+            `}
+          >
+            <img
+              src={item.image}
+              alt={`${item.name}`}
+              css={css`
+                max-width: 100%;
+                margin-right: 16px;
+              `}
+            />
+            <div
+              css={css`
+                min-width: 336px;
+              `}
+            >
+              <p
+                css={css`
+                  font-weight: bold;
+                `}
+              >
+                {item.name}
+              </p>
+              <p
+                css={css`
+                  margin: 16px 0;
+                `}
+              >
+                {item.description}
+              </p>
+              <p>¥{item.price}-</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
