b53a2a30ecd5576f148e76929d27b68bc0d244a5
mp-gumi <basil725woodstock@gmail.com>
2021-08-23 11:29:51 +0900

    Contact　Form装飾のcssをまとめた チェックボックス、ラジオボタンにidとlabelを設定

--- src/component/Contact/ContactBody/index.tsx
+++ src/component/Contact/ContactBody/index.tsx
@@ -3,15 +3,20 @@
 import { jsx, css } from "@emotion/react";
 import { textBlue } from "src/component/Home/HomeBody";
 
-const inquiryList = [
+const kindList = [
   { value: "reservation", displayName: "ご予約" },
   { value: "event", displayName: "イベントについて" },
   { value: "other", displayName: "その他のお問い合わせ" },
 ];
 
-const inquirys = inquiryList.map((inquiry) => {
-  return <option value={`${inquiry.value}`}>{inquiry.displayName}</option>;
+const kinds = kindList.map((kind) => {
+  return <option value={`${kind.value}`}>{kind.displayName}</option>;
 });
+
+const formDecoration = `border-bottom: 1px dotted #bec2c7;
+padding: 12px;
+font-size: 16px;
+line-height: 1.6;`;
 
 export const ContactBody = () => {
   return (
@@ -28,49 +33,48 @@ export const ContactBody = () => {
       >
         お問い合わせ
       </h2>
-      <form
-        css={css`
-          font-size: 16px;
-          line-height: 1.6;
-        `}
-      >
+      <form>
         <div
           css={css`
-            border-bottom: 1px dotted #bec2c7;
-            padding: 12px;
+            ${formDecoration}
           `}
         >
           お問い合わせの種類
           <br />
-          <select name="kind">{inquirys}</select>
+          <select name="kind">{kinds}</select>
         </div>
         <div
           css={css`
-            border-bottom: 1px dotted #bec2c7;
+            ${formDecoration}
           `}
         >
           KUJIRA　Cafeにご来店いただいたことはありますか。
           <br />
-          <input type="radio" name="first" value="yes" checked />
-          はい
-          <input type="radio" name="first" value="no" />
-          いいえ
+          <input type="radio" name="first" value="yes" id="first_yes" checked />
+          <label htmlFor="first_yes">はい</label>
+          <input type="radio" name="first" value="no" id="first_no" />
+          <label htmlFor="first_no">いいえ</label>
         </div>
         <div
           css={css`
-            border-bottom: 1px dotted #bec2c7;
+            ${formDecoration}
           `}
         >
           当カフェをお知りになったきっかけは？
           <br />
-          <input type="checkbox" name="how" value="friends" />
-          知り合いの紹介で
-          <input type="checkbox" name="how" value="magazines" />
-          雑誌・Webサイトで見て
+          <input type="checkbox" name="how" value="friends" id="how_friends" />
+          <label htmlFor="how_friends">知り合いの紹介で</label>
+          <input
+            type="checkbox"
+            name="how"
+            value="magazines"
+            id="how_magazines"
+          />
+          <label htmlFor="how_magazines"> 雑誌・Webサイトで見て</label>
         </div>
         <div
           css={css`
-            border-bottom: 1px dotted #bec2c7;
+            ${formDecoration}
           `}
         >
           お問い合わせの件名
@@ -79,7 +83,7 @@ export const ContactBody = () => {
         </div>
         <div
           css={css`
-            border-bottom: 1px dotted #bec2c7;
+            ${formDecoration}
           `}
         >
           お問い合わせの具体的な内容
