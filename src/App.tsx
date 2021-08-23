abbbe9c4c1f0a476e7c87b5d2bb8b15fa9a7d1aa
mp-gumi <basil725woodstock@gmail.com>
2021-08-23 11:55:38 +0900

    チェックボックス、ラジオボタン ボタン間の距離をpaddingで設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { jsx, css } from "@emotion/react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Home } from "src/component/Home";
 import { About } from "src/component/About";
-import { Access } from "./component/Access";
-import { Menu } from "./component/Menu";
-import { Contact } from "./component/Contact";
+import { Access } from "src/component/Access";
+import { Menu } from "src/component/Menu";
+import { Contact } from "src/component/Contact";
 
 export const maxWidth = "767px";
 
@@ -33,6 +33,7 @@ export const App = () => {
             <Route path="/access" exact component={Access} />
             <Route path="/menu" exact component={Menu} />
             <Route path="/contact" exact component={Contact} />
+
             <Redirect to="/home" />
           </Switch>
         </BrowserRouter>
--- src/component/Contact/ContactBody/index.tsx
+++ src/component/Contact/ContactBody/index.tsx
@@ -51,7 +51,14 @@ export const ContactBody = () => {
           KUJIRA　Cafeにご来店いただいたことはありますか。
           <br />
           <input type="radio" name="first" value="yes" id="first_yes" checked />
-          <label htmlFor="first_yes">はい</label>
+          <label
+            htmlFor="first_yes"
+            css={css`
+              padding-right: 10px;
+            `}
+          >
+            はい
+          </label>
           <input type="radio" name="first" value="no" id="first_no" />
           <label htmlFor="first_no">いいえ</label>
         </div>
@@ -63,14 +70,37 @@ export const ContactBody = () => {
           当カフェをお知りになったきっかけは？
           <br />
           <input type="checkbox" name="how" value="friends" id="how_friends" />
-          <label htmlFor="how_friends">知り合いの紹介で</label>
+          <label
+            htmlFor="how_friends"
+            css={css`
+              padding-right: 10px;
+            `}
+          >
+            知り合いの紹介で
+          </label>
           <input
             type="checkbox"
             name="how"
             value="magazines"
             id="how_magazines"
           />
-          <label htmlFor="how_magazines"> 雑誌・Webサイトで見て</label>
+          <label
+            htmlFor="how_magazines"
+            css={css`
+              padding-right: 10px;
+            `}
+          >
+            雑誌・Webサイトで見て
+          </label>
+          <input type="checkbox" name="how" value="other" id="how_other" />
+          <label
+            htmlFor="how_magazines"
+            css={css`
+              padding-right: 10px;
+            `}
+          >
+            その他
+          </label>
         </div>
         <div
           css={css`
@@ -89,6 +119,13 @@ export const ContactBody = () => {
           お問い合わせの具体的な内容
           <br />
           <textarea name="message"></textarea>
+        </div>
+        <div
+          css={css`
+            ${formDecoration}
+          `}
+        >
+          <input type="submit" name="submit" value="送信" />
         </div>
       </form>
     </div>
